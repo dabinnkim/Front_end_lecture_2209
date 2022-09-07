2a84edb6ee0f4b6be8a6368f32f753d5440231be
KimDaBin <kdabina@naver.com>
2022-09-07 12:18:42 +0900

    hello world

--- 03.JavaScript/02.basic.js
+++ 03.JavaScript/02.basic.js
@@ -1,11 +1,15 @@
 //웬만하면 const로 변수를 선언할것
 //값이 변하는 변수만 var로 선언할것(2015년 이전)
-//left을 사용할것
+//left을 사용할것 (let은 한번밖에 선언이 안됨)
+// 내가 사용할 변수는 미리 정의하고 시작해야만함
+//alt+shift+a -> 주석 단축키
 
-var a =10;
-a+=20;
-console.log(a);
 
-const b=10;
-b+=20;
-console.log(b);
+
+//자동자료형 변환
+console.log(52+'37');
+console.log(1+truem true+'bool');
+
+//일치 연산자
+console.log(1==1.0, 1===1.0)
+console.log(1=='1',1==='1',1!=='1')
